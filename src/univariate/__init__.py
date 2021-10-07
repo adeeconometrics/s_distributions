13112dc5a7194d5f0091e74a60c68b1b230f5ad7
Dave <47932994+adeeconometrics@users.noreply.github.com>
2021-10-08 03:55:37 +0800

    added doc on how to avoid possible name collisions

--- src/univariate/__init__.py
+++ src/univariate/__init__.py
@@ -1,3 +1,9 @@
+"""
+We want to avoid name collisions and keep our names in the module they are defined in. 
+There are two ways we can deal about it:
+- using __all__ = [`DO_NOT_WILD_IMPORT`] to avoid `from [module] import *
+- specify which class you are importing e.g. `from .Base import Base`
+"""
 from .Base import *
 from .Arcsine import *
 from .Cauchy import *
